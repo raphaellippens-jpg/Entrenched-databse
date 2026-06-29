be9e383967fe58556de4a0302e285513f9c93bb0
Raphaellippj <raphaellippens@icloud.com>
2026-06-29 21:43:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1399,6 +1399,38 @@ refresh();
 
 exportDatabase.onclick=()=>{
 
-alert("HELLO");
+const data=JSON.stringify(
+players,
+null,
+2
+);
+
+const blob=new Blob(
+[data],
+{
+type:"application/json"
+}
+);
+
+const url=
+URL.createObjectURL(blob);
+
+const a=
+document.createElement("a");
+
+a.href=url;
+
+a.download=
+"EntrenchedDatabase.json";
+
+a.click();
+
+URL.revokeObjectURL(url);
+
+status.style.color=
+"#16a34a";
+
+status.textContent=
+"Database exported.";
 
 };
